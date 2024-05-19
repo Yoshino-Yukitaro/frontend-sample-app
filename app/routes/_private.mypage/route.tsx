1e663c461e90b980d731728b5d602633bd9050de
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-05-19 12:23:23 +0900

    feat: プライベートなページを作成した

---
+++ app/routes/_private.mypage/route.tsx
@@ -0,0 +1,9 @@
+const MypageRoute = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+    </div>
+  );
+}
+
+export default MypageRoute;
---
+++ app/routes/_private.todos/route.tsx
@@ -0,0 +1,9 @@
+const TodosRoute = () => {
+  return (
+    <div>
+      <h1>Todos</h1>
+    </div>
+  )
+}
+
+export default TodosRoute
